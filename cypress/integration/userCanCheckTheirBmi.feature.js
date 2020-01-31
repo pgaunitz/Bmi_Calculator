4c587d1179c348801021d34dbce5f2e8d1c0d38b
Philip Gaunitz <fluffen@pgaunitz.se>
2020-01-31 14:12:41 +0100

    updating tests

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -4,8 +4,9 @@ describe('User can check their Bmi', () => {
 	it('user can enter weight and height', () => {
 		cy.visit('http://localhost:3001')
 		cy.contains('BMI Calculator')
-		cy.get('#weight').type('90')
-		cy.get('#height').type('186')
+		cy.get('#weight').type('80')
+		cy.get('#height').type('180')
 		cy.get('#Calculate').click()
+		cy.get('#results').should('contain', '24.69')
 	})
 })
